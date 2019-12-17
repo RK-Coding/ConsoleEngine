e4aafa7d108f557a8d7d3c3f09eb18a7b7d4e8a3
RK-Coding <51419924+RK-Coding@users.noreply.github.com>
2019-12-16 19:12:53 -0600

    Update example.py

--- Examples/example.py
+++ Examples/example.py
@@ -1,10 +1,13 @@
-import ConsoleEngine as CE
+import ConsoleEngine as CE #Need to be in same direcory
 
 
 CE.set_title('Smiley Face')
 CE.set_draw_color('2')
 CE.set_size(10, 120)
-Face = CE.Sprite([['1  1'], [' '], ['-------']], 2, 2)
+Face = CE.Sprite([['1  1'], [' '], ['-------']], 2, 2) #Each string in list is a part and each list is a new line
+#'1 1' part 1 line 1
+#' ' part 2 line 2
+#'-------' part 3 line 3
 
 CE.start_up(10, 10)
 while True:
